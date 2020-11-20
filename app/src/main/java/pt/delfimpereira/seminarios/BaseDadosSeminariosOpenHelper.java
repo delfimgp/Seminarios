1b4084d007d067febe0a1ab7220162ce907f79dc
Delfim Pereira <delfimgp@hotmail.com>
2020-11-20 21:29:51 +0000

    Implementação do construtor da base de dsados OpenHelper

---
+++ app/src/main/java/pt/delfimpereira/seminarios/BaseDadosSeminariosOpenHelper.java
@@ -0,0 +1,27 @@
+package pt.delfimpereira.seminarios;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BaseDadosSeminariosOpenHelper extends SQLiteOpenHelper {
+
+    public static final String NOME_BASE_DADOS = "seminarios.db";
+    public static final int VERSAO_BASE_DADOS = 1;
+
+    public BaseDadosSeminariosOpenHelper(@Nullable Context context) {
+        super(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
